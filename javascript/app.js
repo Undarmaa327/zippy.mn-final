7bac414fad563613ad8c3de254656633eb73c38b
munkhbayar21 <68492597+munkhbayar21@users.noreply.github.com>
2020-12-06 20:49:38 +0800

    fresh start

---
+++ javascript/app.js
@@ -0,0 +1,77 @@
+import Product from "./product.js";
+
+const products = [
+    {
+        "id": "1",
+            "productName": "MacBook",
+            "price": "3990000",
+            "brand": "Apple",
+            "img": "./sources/apple-iphone11-3.jpg",
+            "props": [
+                {
+                "Name": "RAM",
+                "Value": "16GB"
+                },
+                {
+                "Name": "CPU",
+                "Value": "M1"
+                },
+                {
+                "Name": "Display",
+                "Value": "Retina"
+                }
+            ]
+    },
+    {
+            "id": "1",
+                "productName": "AJ 11",
+                "brand": "Nike",
+                "img": "./sources/sport-adidas.jpg",
+                "props":  [
+                    {
+                        "Name": "Size",
+                        "Value": "38"
+                        },
+                        {
+                        "Name": "Color",
+                        "Value": "Red"
+                    }
+                ],
+                "defaultPrice": "12314",
+                "pricing": [
+                {
+                    "condition": [{
+                        "Name": "Size",
+                        "Value": "40"
+                        },
+                        {
+                        "Name": "Color",
+                        "Value": "Red"
+                        }],
+                        "price":"23423"
+                },
+                {
+                    "condition": [{
+                        "Name": "Size",
+                        "Value": "38"
+                        },
+                        {
+                        "Name": "Color",
+                        "Value": "Red"
+                        }],
+                        "price":"12345"
+                }
+                ]
+    }
+];
+    
+    for (const product of products){
+
+        const prod= new Product(product.id, product.productName, product.price, product.img, product.brand, product.props)
+
+        document.getElementById("baraaOruulah").insertAdjacentHTML("afterbegin", prod.render());
+
+    }   
+
+
+product.test();
---
+++ javascript/product.js
@@ -0,0 +1,41 @@
+export default class Product{
+    constructor(id, productName, price, img, brand, props){
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.img = img;
+        this.brand = brand;
+        this.props = props;
+    }
+    renderProps(props){
+        let retValue="";
+        for (const prop of props)
+            retValue+= `<div><span style="font-weight:bold;">${prop.Name}</span> : <span>${prop.Value}</span>`;
+
+        return retValue;
+    }
+    SetPrice(newPrice){
+        document.getElementById(this.id)
+        document.querySelector('#'+this.id+'.price').innerHTML = changePrice();
+
+    }
+    render(){
+        return `<div class="product" id="${this.id}">
+        <p class="category-type">ГОО САЙХАН</p>
+        <img src="${this.img}" alt="beauty-perfume-hugo">
+        <h4 class="product-name">${this.productName}</h4>
+        <p class="price">${this.price}₮</p>
+            ${this.renderProps(this.props)}
+        </div>`
+
+    }
+}
+//static method
+// properties dynamic js object
+
+//<div class="product">
+//<p id="category-type">ГОО САЙХАН</p>
+//<img src="/sources/beauty-perfume-hugo.jpg" alt="beauty-perfume-hugo">
+//<h4 id="product-name">HUGO BOSS</h4>
+//<p id="price">419,000₮</p>
+//</div>
